00f95fb50994110f880ee8ef0a712c2446e29f54
Bousdras George <b.giorgos_kar@hotmail.com>
2015-02-12 07:55:50 +0200

    connect_fuctions_

--- SendEmail.py
+++ SendEmail.py
@@ -31,13 +31,13 @@ def Send(to_addr_list, cc_addr_list, subject, message):
     server.sendmail(Sender, receivers_list, message)
     server.quit()
     
-    #write action to log file
+    #write action in log file
     print (datetime.now().strftime('%Y-%m-%d %H:%M:%S')+": Successfully sent email")
     log = open('file.log','a')
     log.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+": \nStatus has chaged\nSuccessfully sent email\n\n")
     log.close()
   except SMTPException:
-    #write action to log file
+    #write action in log file
     print (datetime.now().strftime('%Y-%m-%d %H:%M:%S')+": \nError!! Unable to send email\n\n")
     log = open('file.log','a')
     log.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+": \nError!! Unable to send email\n\n")
